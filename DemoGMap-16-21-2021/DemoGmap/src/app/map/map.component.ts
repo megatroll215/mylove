7d33005944373338928415429c5a6deeeb30a9d8
ANhTuan <tuan2461995@gmail.com>
2021-12-21 15:33:56 +0700

    21-12-2021

--- DemoGMap-16-21-2021/DemoGmap/src/app/map/map.component.ts
+++ DemoGMap-16-21-2021/DemoGmap/src/app/map/map.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, forwardRef, OnInit} from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
+import {default as data} from 'data.json';
 
 @Component({
   selector: 'app-map',
@@ -9,14 +10,28 @@ import { Loader } from '@googlemaps/js-api-loader';
 export class MapComponent implements OnInit {
 
 
+  displayInfo(info: google.maps.InfoWindow): void{
+    info.open();
+  }
+  ColorIntension(population: any): string{
+    if(population == null || population<0) return "#ffffff";
+     else if(population>500) return " #ff3300";
+     else if(population>400 && population<=500) return " #ff6600";
+     else if(population>300 && population<=400) return " #ff9933";
+     else if(population>200 && population<=300) return " #ffcc00";
+     else if(population>100 && population<=200) return " #ffff66";
+     else return " #aaff80";
+  }
   ngOnInit(): void {
+
+
     const Hanoi: any = {
-      lat: 21.028511,
-      lng: 105.804817,
+      lat: 21.028033,
+      lng: 105.851242,
     };
 
     const mapOptions = {
-      zoom: 8,
+      zoom: 15,
       center: Hanoi,
     };
 
@@ -24,101 +39,53 @@ export class MapComponent implements OnInit {
       document.getElementById('map') as HTMLElement,
       mapOptions
     );
-
-    let zone_01 = new google.maps.Marker({
-      position: {
-        lat: 21.02685574744757,
-        lng: 105.85730111936786 ,
-      },
-
      
-      map: map,
-    });
-
-    const info1 = new google.maps.InfoWindow({
-      content: "<p>Layer :&nbsp 1;</p>"
-      +"<p>BinCount:</p>1"
-      +"<p>MtdCount:</p>1"
-      +"<p>Population: &nbsp60.49230769230801;</p>"
-    })
-
-    zone_01.addListener("click", () => {
-      info1.open({
-        anchor: zone_01,
-        map,
-        shouldFocus: false,
-      });
-    });
-
-    const zone_01data = [
-      { lat: 21.028654384565524, lng: 105.85841365901365,},
-      { lat: 21.026855732977843, lng: 105.85952617180808, },
-      { lat: 21.02505710309476, lng: 105.85841363216345,  },
-      { lat: 21.02505710309476, lng: 105.85618860657226 ,},
-      { lat: 21.026855732977843, lng: 105.85507606692762 ,},
-      { lat: 21.028654384565524, lng: 105.85618857972206  ,},
-      { lat: 21.028654384565524, lng: 105.85841365901365 ,},
-    ];
-  
-    // Construct the polygon.
-    const zone_01_area = new google.maps.Polygon({
-      paths: zone_01data,
-      strokeColor: "#FF0000",
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: "#FF0000",
-      fillOpacity: 0.35,
-    });
-  
-    zone_01_area.setMap(map);
+//end zone 3
+    // Create a <script> tag and set the USGS URL as the source.
 
 
-    let zone_02 = new google.maps.Marker({
-      position: {
-        lat: 21.035849113890595,
-        lng: 105.86063845639892 ,
-      },
-      
-    map:map
-    });
+   for(let i = 0; i<data.zone.length;i++){
+     const center = data.zone[i].center;
+     const location = new google.maps.LatLng(center.lat,center.lng);
 
-    const info2 = new google.maps.InfoWindow({
-      content: "<p>Layer :&nbsp 1;</p>"
-      +"<p>BinCount:</p>0"
-      +"<p>MtdCount:</p>null"
-      +"<p>Population: &nbsp null;</p>"
-    })
 
-    zone_02.addListener("click", () => {
-      info2.open({
-        anchor: zone_02,
-        map,
-        shouldFocus: false,
-      });
-    });
+     const info = new google.maps.InfoWindow({
+       content: "<p>Layer :&nbsp " + data.zone[i].layer+ "</p>"
+         +"<p>BinCount: "+data.zone[i].binCount+"</p>"
+         +"<p>MtdCount:"+data.zone[i].mtdCount+"</p>"
+         +"<p>Population: &nbsp"+ data.zone[i].population+"</p>",
 
-    const zone_02data = [
-      { lat: 21.03764775100685, lng: 105.86175106319631,},
-      { lat: 21.035849099414083, lng: 105.86286364312811, },
-      { lat: 21.034050469536087, lng: 105.8617510363319,  },
-      { lat: 21.034050469536087, lng: 105.85952587646595 ,},
-      { lat: 21.035849099414083, lng: 105.85841326966973 ,},
-      { lat: 21.03764775100685, lng: 105.85952584960154  ,},
-      { lat: 21.03764775100685, lng: 105.86175106319631 ,},
-    ];
-  
-    // Construct the polygon.
-    const zone_02_area = new google.maps.Polygon({
-      paths: zone_02data,
-      strokeColor: "#FF0000",
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: "#grey",
-      fillOpacity: 0.35,
-    });
+       position: center
+     })
 
-    zone_02_area.setMap(map);
 
-    
+    let hexagons = data.zone[i].hexagon;
+     let zone_data :google.maps.LatLng[]|undefined;
+
+
+
+     const zone_area = new google.maps.Polygon({
+       paths: hexagons,
+       strokeColor: "#FF0000",
+       strokeOpacity: 0.8,
+       strokeWeight: 2,
+       fillColor: this.ColorIntension(data.zone[i].population),
+       fillOpacity: 0.35,
+     });
+
+     google.maps.event.addListener(zone_area,"click",function(){
+       info.open(map);
+     });
+
+     zone_area.setMap(map);
+
+    }
 }
-}
+
+  }
+
+
+// Loop through the results array and place a marker for each
+// set of coordinates.
+
+
