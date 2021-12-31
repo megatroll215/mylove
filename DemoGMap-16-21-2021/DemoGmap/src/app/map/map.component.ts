7700a70b835bcc210956a4d1b97f2fa7fc411b45
ANhTuan <tuan2461995@gmail.com>
2021-12-31 16:53:09 +0700

    31-12-2021

--- DemoGMap-16-21-2021/DemoGmap/src/app/map/map.component.ts
+++ DemoGMap-16-21-2021/DemoGmap/src/app/map/map.component.ts
@@ -31,6 +31,8 @@ export class Poly implements Area {
 
 }
 
+
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -39,9 +41,9 @@ export class Poly implements Area {
 
 
 export class MapComponent implements OnInit, Array {
-
-locationService: LocationService|any
-http: HttpClient | any
+  infoWindow = new google.maps.InfoWindow
+  locationService: LocationService | any
+  http: HttpClient | any
   tempContent: any
   poly: any[] = [];
   info: any
@@ -61,47 +63,44 @@ http: HttpClient | any
 //get GeoCoder
 
 
+  /*    let geocoder = new google.maps.Geocoder();*/
 
+  /* geocoder.geocode({location: location}).then((response)=>{
+   // @ts-ignore
+     if(response.results[0] )
+   {
+     detail =  response.results[0].formatted_address.toString()
+     console.log("1 : " + response.results[0].formatted_address.toString())
+     return detail
 
-/*    let geocoder = new google.maps.Geocoder();*/
+   }else return detail}
 
-    /* geocoder.geocode({location: location}).then((response)=>{
-     // @ts-ignore
-       if(response.results[0] )
-     {
-       detail =  response.results[0].formatted_address.toString()
-       console.log("1 : " + response.results[0].formatted_address.toString())
-       return detail
+  )*/
 
-     }else return detail}
+  /*geocoder.geocode({'location': location}, (results, status) => {
+    if (status == 'OK') {
+      if (results) {
 
-    )*/
-
-    /*geocoder.geocode({'location': location}, (results, status) => {
-      if (status == 'OK') {
-        if (results) {
-
-          this.locationDetail += results[0].formatted_address.toString()
+        this.locationDetail += results[0].formatted_address.toString()
 
 
 
-        }
-
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
       }
 
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
 
-    }).then(() =>{
-      new google.maps.Marker({
-        position: location,
-        map: this.map,
-        title: this.locationDetail,
 
-      })
+  }).then(() =>{
+    new google.maps.Marker({
+      position: location,
+      map: this.map,
+      title: this.locationDetail,
+
     })
+  })
 */
-
 
 
   //end get geocoder
@@ -121,16 +120,47 @@ http: HttpClient | any
 
   }
 
+ handleLocationError(this: any, browserHasGeolocation: boolean,
+                               infoWindow: google.maps.InfoWindow,
+                               pos: google.maps.LatLng) {
+    {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(
+        browserHasGeolocation
+          ? "Error: The Geolocation service failed."
+          : "Error: Your browser doesn't support geolocation."
+      );
+      infoWindow.open(this.map);
+    }
+
+  }
+
 
   async TranslateTo(lang: string) {
     this.translate.use(lang)
 
 
   }
-  async  CreateElement(elem: string){
 
 
+
+  CreateButton(btnName: string, btnTitle: string, btnTd: string, pos: google.maps.ControlPosition) {
+    const centerDiv = document.createElement("div") as HTMLElement
+    centerDiv.className = "d-flex justify-content-center"
+    const centerButton = document.createElement("button") as HTMLElement;
+    centerButton.textContent = btnName
+    centerButton.title =
+      centerButton.id = "center-button"
+    centerButton.style.fontFamily = "Monospace"
+    centerButton.style.fontWeight = "bold"
+    centerButton.className = "btn btn-light"
+    centerDiv.style.marginTop = "10px"
+    centerDiv.style.marginBottom = "10px"
+
+    centerDiv.appendChild(centerButton)
+    this.map.controls[pos].push(centerDiv)
   }
+
   ColorIntensive(population: any, layer: number): string {
     switch (layer) {
       case 1:
@@ -171,117 +201,114 @@ http: HttpClient | any
 
 
   async ngOnInit() {
-
+//mapInit
     this.map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
       zoom: 14,
       center: this.Hanoi,
       zoomControl: true,
+      zoomControlOptions:{
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
+      },
       mapTypeControl: true,
-      mapTypeControlOptions:{
-        mapTypeIds:["roadmap", "satellite", "hybrid", "terrain","dark-mode"],
-       style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      mapTypeControlOptions: {
+        mapTypeIds: ["roadmap", "satellite", "hybrid", "terrain", "dark-mode"],
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
         position: google.maps.ControlPosition.LEFT_TOP
 
       },
       rotateControl: true,
-      scaleControlOptions:{
-        style: ScaleControlStyle.DEFAULT
+      scaleControlOptions: {
+
       },
 
 
-
-
-
-
-
     });
-
+//end
     //Dark-Mode styled
-    const darkMode = new google.maps.StyledMapType( [
-      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+    const darkMode = new google.maps.StyledMapType([
+      {elementType: "geometry", stylers: [{color: "#242f3e"}]},
+      {elementType: "labels.text.stroke", stylers: [{color: "#242f3e"}]},
+      {elementType: "labels.text.fill", stylers: [{color: "#746855"}]},
       {
         featureType: "administrative.locality",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
+        stylers: [{color: "#d59563"}],
       },
       {
         featureType: "poi",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
+        stylers: [{color: "#d59563"}],
       },
       {
         featureType: "poi.park",
         elementType: "geometry",
-        stylers: [{ color: "#263c3f" }],
+        stylers: [{color: "#263c3f"}],
       },
       {
         featureType: "poi.park",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#6b9a76" }],
+        stylers: [{color: "#6b9a76"}],
       },
       {
         featureType: "road",
         elementType: "geometry",
-        stylers: [{ color: "#38414e" }],
+        stylers: [{color: "#38414e"}],
       },
       {
         featureType: "road",
         elementType: "geometry.stroke",
-        stylers: [{ color: "#212a37" }],
+        stylers: [{color: "#212a37"}],
       },
       {
         featureType: "road",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#9ca5b3" }],
+        stylers: [{color: "#9ca5b3"}],
       },
       {
         featureType: "road.highway",
         elementType: "geometry",
-        stylers: [{ color: "#746855" }],
+        stylers: [{color: "#746855"}],
       },
       {
         featureType: "road.highway",
         elementType: "geometry.stroke",
-        stylers: [{ color: "#1f2835" }],
+        stylers: [{color: "#1f2835"}],
       },
       {
         featureType: "road.highway",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#f3d19c" }],
+        stylers: [{color: "#f3d19c"}],
       },
       {
         featureType: "transit",
         elementType: "geometry",
-        stylers: [{ color: "#2f3948" }],
+        stylers: [{color: "#2f3948"}],
       },
       {
         featureType: "transit.station",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
+        stylers: [{color: "#d59563"}],
       },
       {
         featureType: "water",
         elementType: "geometry",
-        stylers: [{ color: "#17263c" }],
+        stylers: [{color: "#17263c"}],
       },
       {
         featureType: "water",
         elementType: "labels.text.fill",
-        stylers: [{ color: "#515c6d" }],
+        stylers: [{color: "#515c6d"}],
       },
       {
         featureType: "water",
         elementType: "labels.text.stroke",
-        stylers: [{ color: "#17263c" }],
+        stylers: [{color: "#17263c"}],
       },
-    ],{name:"Night Mode"})
+    ], {name: "Night"})
 
-    this.map.mapTypes.set("dark-mode",darkMode)
+    this.map.mapTypes.set("dark-mode", darkMode)
     this.map.setMapTypeId("dark-mode")
     //end
-
 
 
 //Legend
@@ -296,39 +323,66 @@ http: HttpClient | any
 
     LegendContainer.style.textAlign = "center";
     LegendContainer.title = "Population color";
+    LegendContainer.style.marginTop = "10px"
+    LegendContainer.style.marginBottom = "10px"
     const LegendHeader = document.createElement("p") as HTMLElement
-    LegendHeader.textContent = "Legend"
+    LegendHeader.textContent = ""
     LegendHeader.style.fontWeight = "bold"
-    LegendHeader.style.backgroundColor= "transparent"
+    LegendHeader.style.backgroundColor = "transparent"
     LegendHeader.style.color = "yellow"
-    LegendHeader.style.fontFamily ="Courier"
+    LegendHeader.style.fontFamily = "Courier"
     LegendContainer.appendChild(LegendHeader);
-    const hr =document.createElement("hr") as HTMLElement
+    const hr = document.createElement("hr") as HTMLElement
     hr.style.backgroundColor = "white"
     LegendContainer.appendChild(hr);
-    const ref = document.createElement("div");
-    const redBox =document.createElement("input");
+
 
     (document.getElementById("holder") as HTMLElement).append(LegendContainer)
+
+//center-button
+    this.CreateButton("Center", "move back to center", "btnCenter", google.maps.ControlPosition.TOP_CENTER)
+    //end
+
+//GPS-button
+    this.CreateButton("GPS", "your location", "btnGPS", google.maps.ControlPosition.RIGHT_CENTER);
+//end
+//GPS-button-event
+/*    (document.getElementById("btnGPS") as HTMLElement).addEventListener("click", () => {
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position: GeolocationPosition) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+
+            this.infoWindow.setPosition(pos);
+            this.infoWindow.setContent("Location found.");
+            this.infoWindow.open(this.map);
+            this.map.setCenter(pos);
+          },
+          () => {
+            this.handleLocationError(true, this.infoWindow, this.map.getCenter()!);
+          }
+        );
+      } else {
+        // Browser doesn't support Geolocation
+        this.handleLocationError(false, this.infoWindow, this.map.getCenter()!);
+      }
+    });*/
 
 
 
 
 //end
+
+    //For: hexagons
     for (let i = 0; i < data.zone.length; i++) {
 
 
-
-      if (data.zone[i].population !== null && <number>data.zone[i].population>=100) {
+      if (data.zone[i].population !== null && <number>data.zone[i].population >= 100) {
         const center = data.zone[i].center as unknown as google.maps.LatLng;
-
-
-
-//<div class="col-md 3">
-//   <div class="d-flex justify-content-center"><button title="move back to center" id="center-button" style="position: absolute;z-index: 1;font-family: monospace;font-weight:bold " class="btn btn-light"  >CENTER</button></div>
-
-
-
 
 
 
@@ -360,7 +414,6 @@ http: HttpClient | any
         this.locationDetail = res.data.results[0].formatted_address
 
 
-
         this.tempContent = "<b>" + await this.TranslateTool("ZONE-INFO.layer") + "</b>" + " : " + "<span>" + data.zone[i].layer + "</span>" + "</br>" +
           "<b>" + await this.TranslateTool("ZONE-INFO.BIN-COUNT") + "</b>" + " : " + "<span>" + data.zone[i].binCount + "</span>" + "</br>" +
           "<b>" + await this.TranslateTool("ZONE-INFO.MTD-COUNT") + "</b>" + " : " + "<span>" + data.zone[i].mtdCount + "</span>" + "</br>" +
@@ -383,12 +436,9 @@ http: HttpClient | any
           info.setContent("<b>" + await this.TranslateTool("ZONE-INFO.layer") + "</b>" + " : " + "<span>" + data.zone[i].layer + "</span>" + "</br>" +
             "<b>" + await this.TranslateTool("ZONE-INFO.BIN-COUNT") + "</b>" + " : " + "<span>" + data.zone[i].binCount + "</span>" + "</br>" +
             "<b>" + await this.TranslateTool("ZONE-INFO.MTD-COUNT") + "</b>" + " : " + "<span>" + data.zone[i].mtdCount + "</span>" + "</br>" +
-            "<b>" + await this.TranslateTool("ZONE-INFO.POPULATION") + "</b>" + " : " + "<span>" + pop + "</span></br>"+ this.locationDetail )
+            "<b>" + await this.TranslateTool("ZONE-INFO.POPULATION") + "</b>" + " : " + "<span>" + pop + "</span></br>" + this.locationDetail)
 
         })
-
-
-
 
 
         zone_area.setMap(this.map);
@@ -406,6 +456,8 @@ http: HttpClient | any
               this.poly[i].polygon.setMap(this.map)
             }
           }
+          ;
+          this.map.setCenter({lat: 21.029014, lng: 105.851083} as unknown as google.maps.LatLng)
         })
         google.maps.event.addDomListener(document.getElementById("layer2_show") as HTMLElement, "click", () => {
           for (let i = 0; i < this.poly.length; i++) {
@@ -418,6 +470,8 @@ http: HttpClient | any
               this.poly[i].polygon.setMap(this.map)
             }
           }
+          ;
+          this.map.setCenter({lat: 21.029014, lng: 105.851083} as unknown as google.maps.LatLng)
         })
         google.maps.event.addDomListener(document.getElementById("layer3_show") as HTMLElement, "click", () => {
           for (let i = 0; i < this.poly.length; i++) {
@@ -430,6 +484,8 @@ http: HttpClient | any
               this.poly[i].polygon.setMap(this.map)
             }
           }
+          ;
+          this.map.setCenter({lat: 21.029014, lng: 105.851083} as unknown as google.maps.LatLng)
         })
 
         google.maps.event.addDomListener(document.getElementById("All_layer_show") as HTMLElement, "click", () => {
@@ -438,6 +494,8 @@ http: HttpClient | any
 
             this.poly[i].polygon.setMap(this.map)
           }
+          ;
+          this.map.setCenter({lat: 21.029014, lng: 105.851083} as unknown as google.maps.LatLng)
 
         })
         google.maps.event.addDomListener(document.getElementById("All_layer_hidden") as HTMLElement, "click", () => {
@@ -446,10 +504,10 @@ http: HttpClient | any
 
             this.poly[i].polygon.setMap(this.map)
           }
+          ;
+          this.map.setCenter({lat: 21.029014, lng: 105.851083} as unknown as google.maps.LatLng)
 
         })
-
-
 
 
       }
@@ -460,28 +518,13 @@ http: HttpClient | any
 
     }
     //create Center-button
-    google.maps.event.addDomListener(document.getElementById("center-button") as HTMLElement,"click", ()=>{
+    google.maps.event.addDomListener(document.getElementById("center-button") as HTMLElement, "click", () => {
       this.map.setCenter(this.Hanoi);
       this.map.setZoom(14.5);
     })
-    //
-
-    //find your location button
-
-    //end
-
-    //Create Legend table
-
-    //end
-
-
-
-
 
 
     //
-
-
 
 
   }
